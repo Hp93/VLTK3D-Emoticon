1e5cf186abe7761b263198d8cadca3267921360c
Hp93 <Hp93.Dev@outlook.com>
2015-06-22 21:13:34 +0700

    Init

---
+++ Firefox/data/FacebookStructureNote.js
@@ -0,0 +1,38 @@
+﻿/*
+ * VLTK3D Emoticon
+ * Version: 1.0
+ * Author: Hp93
+ * License: Mozilla Public License, v. 2.0 @ https://www.mozilla.org/MPL/2.0/
+ * Description: This file describe the overal structure of a Facebook Message Node.
+ *              Include main Tags and its Classes.
+ */
+
+//<body>
+//    ...
+//    <div class="conversation">
+//        <div>                                             << Conversation node
+//            <div class="._5w-5"></div>
+//            <div class="_5wd4 ..."></div>
+//            <div class="._5w-5"></div>
+//            <div class="_5wd4 ..."></div>
+//            <div class="_5wd4 ...">                       << Message node
+//                <div class="_5wd9">
+//                    ...
+//                    <div class="_5yl5">                   << Content node
+//                        <span>
+//                            <span>
+//                                #text abc
+//                                <span>icon text</span>
+//                                <span>icon image</span>
+//                            </span>
+//                        </span>
+//                    </div>
+//                    ...
+//                </div>
+//                ...
+//            </div>
+//            ...
+//        </div>
+//    </div>
+//    ...
+//</body>
---
+++ Firefox/lib/main.js
@@ -0,0 +1,79 @@
+/*
+ * VLTK3D Emoticon
+ * Version: 1.0
+ * Author: Hp93
+ */
+
+"use strict";
+
+//#region===== References =======================
+
+var buttons = require('sdk/ui/button/action');
+var self = require("sdk/self");
+var pageMods = require("sdk/page-mod");
+var notifications = require("sdk/notifications");
+
+//#endregion
+
+//#region===== Property =========================
+
+var lblActivate = "Activate VLTK3D Emoticon";
+var lblDeactivate = "Deactivate VLTK3D Emoticon";
+var button = buttons.ActionButton({
+    id: "my-button",
+    label: lblDeactivate,
+    icon: {
+        "16": "./icon-16.png",
+        "32": "./icon-32.png",
+        "64": "./icon-64.png"
+    },
+    onClick: destroyPageMode,
+    jx3State: false
+});
+var pageMod = pageMods.PageMod({
+    include: "*.facebook.com",
+    contentScriptFile: [self.data.url("VLTK3D-Emoticon.js")],
+    contentStyleFile: [self.data.url("EmoStyle.css")]
+});
+
+//#endregion
+
+//#region===== Main =============================
+
+function registerPageMode() {
+
+    pageMod = pageMods.PageMod({
+        include: "*.facebook.com",
+        contentScriptFile: [self.data.url("VLTK3D-Emoticon.js")],
+        contentStyleFile: [self.data.url("EmoStyle.css")]
+    });
+
+    // Switch button state
+    button.removeListener("click", registerPageMode);
+    button.on("click", destroyPageMode);
+    button.label = lblDeactivate;
+
+    notifications.notify({
+        title: "VLTK3D Emoticon disabled",
+        text: "Emoticon has been enabled, please refresh website to take effect.",
+        data: "enabled"
+    });
+}
+
+function destroyPageMode() {
+
+    pageMod.destroy();
+
+    // Switch button state
+    button.removeListener("click", destroyPageMode);
+    button.on("click", registerPageMode);
+    button.label = lblActivate;
+
+    notifications.notify({
+        title: "VL3D Emoticon enabled",
+        text: "Emoticon has been disabled, please refresh website to take effect.",
+        data: "disabled"
+    });
+}
+
+//#endregion
